37555a7783099f332e159198db76dfb61ec2f05f
jthomas0786 <jthomas0786@gmail.com>
2026-09-25 20:36:36 -0500

    Reuse shared scheduler authorization

--- server/api/x-scheduler.js
+++ server/api/x-scheduler.js
@@ -1,22 +1,5 @@
-const crypto=require('crypto');
 const xWorker=require('./x-worker');
-
-const SCHEDULER_SECRET_SHA256='5f12ee2344aadd169ae6fae8f09a376104e49fc302865e1f9ffc788355a024c6';
-
-function hashSecret(value){
-  return crypto.createHash('sha256').update(String(value||'')).digest('hex');
-}
-
-function safeHashEqual(left,right){
-  const a=Buffer.from(String(left||''));
-  const b=Buffer.from(String(right||''));
-  return a.length===b.length&&crypto.timingSafeEqual(a,b);
-}
-
-function schedulerAuthorized(req,expectedHash=SCHEDULER_SECRET_SHA256){
-  const supplied=req?.headers?.['x-parlayping-scheduler-secret'];
-  return Boolean(supplied)&&safeHashEqual(hashSecret(supplied),expectedHash);
-}
+const {schedulerAuthorized,hashSecret}=require('./lib/scheduler-auth');
 
 module.exports=async function handler(req,res){
   res.setHeader('Cache-Control','no-store');
